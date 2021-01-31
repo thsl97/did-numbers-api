320e6c7a2fdde605a4c31a2f7939403d0b34e95e
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2021-01-31 14:49:47 -0300

    Moves validation from blueprints to model validations

--- did_numbers/did_numbers_api.py
+++ did_numbers/did_numbers_api.py
@@ -15,7 +15,8 @@ def get_did_numbers():
     page = int(request.args.get('page', 1))
     # offsets the query by the page number and limit the results
     did_numbers = db_session.query(DIDNumber).offset((page - 1) * 25).limit(25)
-    return jsonify({'data': [object.serialized for object in did_numbers]})
+    return jsonify([object.serialized for object in did_numbers])
+
 
 
 @bp.route('/new', methods=('POST',))
@@ -23,39 +24,29 @@ def create_did_number():
     """Creates new DID numbers"""
     data = request.get_json()
     # returns bad request if any DID number field is empty
-    for field, value in data.items():
-        if not value:
+    fields = ['value', 'monthlyPrice', 'setupPrice', 'currency']
+    for field in fields:
+        if field not in data:
             return jsonify(
                 {'message': f'DID Number {field} must be informed'}
             ), 400
-    phone_pattern = re.compile(
-        r'^\+(\d{2}) (\d{2}) (\d{5})-(\d{4})$'
-    )
-    if not phone_pattern.match(data['value']):
-        return jsonify(
-                {'message': 'DID Number value must be in correct format'}
-            ), 400
-    # checks if monthly and setup price are really numbers
-    for field in ['monthlyPrice', 'setupPrice']:
-        try:
-            int(data[field] * 100)
-        except ValueError:
-            return jsonify(
-                {'message': 'Incorrect format for {field}'}
-            ), 400
-    new_number = DIDNumber(
-        value=data['value'],
-        monthly_price=data['monthlyPrice'] * 100,
-        setup_price=data['setupPrice'] * 100,
-        currency=data['currency']
-    )
-    db_session.add(new_number)
-    db_session.commit()
-    return jsonify({'data': new_number.serialized}), 201
+    try:
+        new_number = DIDNumber(
+            value=data['value'],
+            monthly_price=data['monthlyPrice'] * 100,
+            setup_price=data['setupPrice'] * 100,
+            currency=data['currency']
+        )
+        db_session.add(new_number)
+        db_session.commit()
+        return jsonify({'data': new_number.serialized}), 201
+    except AssertionError as e:
+        return jsonify(str(e)), 400
 
 
 @bp.route('/<int:id>/update', methods=('PATCH',))
 def update_did_number(id):
+    """Updates an existing did number"""
     did_number = db_session.query(DIDNumber).get(id)
     if did_number is None:
         return jsonify(
@@ -64,44 +55,24 @@ def update_did_number(id):
     else:
         data = request.get_json()
         if 'value' in data:
-            phone_pattern = re.compile(
-                r'^\+(\d{2}) (\d{2}) (\d{5})-(\d{4})$'
-            )
-            if not phone_pattern.match(data['value']):
-                return jsonify(
-                        {'message': 'DID Number value must be in correct '
-                                    'format'}
-                    ), 400
             did_number.value = data['value']
         if 'monthlyPrice' in data:
-            try:
-                int(data['monthlyPrice'] * 100)
-                did_number.monthly_price = data['monthlyPrice']
-            except ValueError:
-                return jsonify(
-                    {'message': 'Incorrect format for monthlyPrice'}
-                ), 400
+            did_number.monthly_price = data['monthlyPrice'] * 100
         if 'setupPrice' in data:
-            try:
-                int(data['setupPrice'] * 100)
-                did_number.setup_price = data['setupPrice']
-            except ValueError:
-                return jsonify(
-                    {'message': 'Incorrect format for setupPrice'}
-                ), 400
+            did_number.setup_price = data['setupPrice'] * 100
         if 'currency' in data:
-            if not data['currency']:
-                return jsonify(
-                    {'message': 'Currency must not be empty'}
-                ), 400
             did_number.currency = data['currency']
-        db_session.add(did_number)
-        db_session.commit()
-        return jsonify({'data': did_number.serialized})
+        try:
+            db_session.add(did_number)
+            db_session.commit()
+            return jsonify(did_number.serialized)
+        except AssertionError as e:
+            return jsonify(str(e)), 400
 
 
 @bp.route('/<int:id>/delete', methods=('DELETE',))
 def delete_did_number(id):
+    """Deletes an did number"""
     did_number = db_session.query(DIDNumber).get(id)
     if did_number is None:
         return jsonify(
--- did_numbers/models.py
+++ did_numbers/models.py
@@ -1,4 +1,7 @@
+import re
+
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
 from did_numbers.database import Base
 
 
@@ -22,3 +25,19 @@ class DIDNumber(Base):
             'setupPrice': self.setup_price / 100,
             'currency': self.currency
         }
+
+    @validates('value')
+    def validate_value(self, key, address):
+        """Checks that phone number matches pattern"""
+        # +00 00 00000-0000
+        phone_pattern = re.compile(
+            r'^\+(\d{2}) (\d{2}) (\d{5})-(\d{4})$'
+        )
+        assert phone_pattern.match(address) is True, ('Value does not match '
+                                                      'correct pattern')
+        return address
+
+    @validates('monthly_price', 'setup_price')
+    def validate_prices(self, key, address):
+        assert address > 0, 'Prices must be positive numbers'
+        return address
