c6fc7e509397b1db160551444c2ef8b415bdc489
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2021-01-31 10:17:51 -0300

    Returns paginated results from get_did_numbers

--- did_numbers/did_numbers_api.py
+++ did_numbers/did_numbers_api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
 
 from did_numbers.database import db_session
 from did_numbers.models import DIDNumber
@@ -9,6 +9,8 @@ bp = Blueprint('did-numbers', __name__, url_prefix='/did-numbers')
 
 @bp.route('', methods=('GET',))
 def get_did_numbers():
-    """Gets a list of all did numbers"""
-    did_numbers = db_session.query(DIDNumber).all()
+    """Gets a list of all did numbers, paginated by 25"""
+    page = int(request.args.get('page', 1))
+    # offsets the query by the page number and limit the results
+    did_numbers = db_session.query(DIDNumber).offset((page - 1) * 25).limit(25)
     return jsonify({'data': [object.serialized for object in did_numbers]})
